481f706c979aee1cf4f96384950d917c2988e10d
gamename <tennis.n.smith@gmail.com>
2024-10-12 12:33:15 -0500

    add periodic message when updating the firmware

--- gecl-ota-manager.c
+++ gecl-ota-manager.c
@@ -192,6 +192,7 @@ void ota_task(void *pvParameter)
         goto ota_end;
     }
 
+    int iteration = 0;
     while (1)
     {
         err = esp_https_ota_perform(https_ota_handle);
@@ -203,6 +204,11 @@ void ota_task(void *pvParameter)
         // monitor the status of OTA upgrade by calling esp_https_ota_get_image_len_read, which gives length of image
         // data read so far.
         // ESP_LOGD(TAG, "Image bytes read: %d", esp_https_ota_get_image_len_read(https_ota_handle));
+        if (iteration % 10 == 0)
+        {
+            ESP_LOGI(TAG, "Bytes read so far: %d", esp_https_ota_get_image_len_read(https_ota_handle));
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 
     if (esp_https_ota_is_complete_data_received(https_ota_handle) != true)
